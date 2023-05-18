b3373a01698e6bacf2df6cd4ae4c55afb3acf602
Pedro <pedrooliveiradevs@gmail.com>
2023-05-18 13:46:35 -0300

    Component adicionado e configurado

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], function (Controller, MessageToast, JSONModel, ResourceModel) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-      onInit : function () {
-         var oData = {
-            recipient : {
-               name : "World"
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.getView().setModel(oModel);
-
-         var i18nModel = new ResourceModel({
-            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-         });
-         this.getView().setModel(i18nModel, "i18n");
-      },
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
          var oBundle = this.getView().getModel("i18n").getResourceBundle();
          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -27,5 +11,5 @@ sap.ui.define([
          
          MessageToast.show(sMsg);
       }
-    });
- });
+   });
+});
