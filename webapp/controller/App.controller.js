734d6f8c2f21c1a66edf5d8bdd9f075ebd8bb04d
Pedro <pedrooliveiradevs@gmail.com>
2023-05-18 12:22:18 -0300

    Adicionado i18n propriedades e suas devidas funções no controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,8 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
- ], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/resource/ResourceModel"
+ ], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onInit : function () {
@@ -11,12 +12,20 @@ sap.ui.define([
                name : "World"
             }
          };
-         
          var oModel = new JSONModel(oData);
          this.getView().setModel(oModel);
+
+         var i18nModel = new ResourceModel({
+            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+         });
+         this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello : function () {
-         MessageToast.show("Hello World");
-       }
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+         
+         MessageToast.show(sMsg);
+      }
     });
  });
