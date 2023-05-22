a97e1866339f54a45ffda5a29f67c19019c7de31
Pedro <pedrooliveiradevs@gmail.com>
2023-05-22 12:16:42 -0300

    Opção de fechar a caixa de dialogo adicionada

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -20,6 +20,9 @@ sap.ui.define([
           this.pDialog.then(function(oDialog) {
              oDialog.open();
           });
-       }
+       },
+       onCloseDialog : function () {
+			this.byId("helloDialog").close();
+		}
     });
- });
+ });  
